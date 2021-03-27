91c32d8b9dcc860994c064eb3c5be9f33f2bdad9
emma-l-myoosik <emma.li.22@bishops.com>
2021-03-27 16:49:22 -0700

    basically did index.html; modified registration; started model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Patient(models.Model):
+    Name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Name
+
+class Address(models.Model):
+    name = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    address1 = models.CharField(max_length=300)
+    address2 = models.CharField(max_length=300)
+    email = models.CharField(max_length=50)
+    ph = models.IntegerField()
+    contact = models.BooleanField()
+
+    def __str__(self):
+        return self.email
