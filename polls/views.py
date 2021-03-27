2dd0b2528717e92d6e2ff06470e67b822845ba39
emma-l-myoosik <emma.li.22@bishops.com>
2021-03-26 21:09:01 -0700

    created templates, polls, and index.html

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    return HttpResponse("its the first text of cvclinic")
+    context = {}
+    return render(request, 'polls/index.html', context)
